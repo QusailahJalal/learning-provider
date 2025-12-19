aeb2869db31972c83ac27228d4c36870669825e3
Jalal Qusailah <132620295+QusailahJalal@users.noreply.github.com>
2025-12-19 23:15:50 +0300

    feat: Refactor HomeView to improve counter management and UI interaction

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(DevicePreview(
-    builder: (context) {
-      return const MyApp();
-    }
-  ));
+  runApp(
+    DevicePreview(
+      builder: (context) {
+        return const MyApp();
+      },
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -27,39 +29,133 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  ChangeNotifierProvider(
+    return ChangeNotifierProvider(
       create: (context) => HomeViewProvider(),
 
-      child :Scaffold(
-        appBar: AppBar(
-          title: Text('Home View'),
-        ),
+      child: Scaffold(
+        appBar: AppBar(title: Text('Home View')),
         body: Center(
-          child: Consumer<HomeViewProvider>(builder: (context, homeProvider, child) => Column(
+          child: Column(
+            mainAxisAlignment: .center,
             children: [
-              SizedBox(height: 30,),
-              Text(homeProvider.helloMessage),
-              SizedBox(height: 30,),
-              ElevatedButton(onPressed: () {
-                    homeProvider.updateMessage('Hello again, World!');
-              }, child: Text('Next View'))
+              Selector<HomeViewProvider, int>(
+                builder: (context, value, child) {
+                  print('Incrementing Counter1');
+                  return Text(
+                    value.toString(),
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  );
+                },
+                selector: (context, provider) => provider.counter1,
+              ),
+              Selector<HomeViewProvider, int>(
+                builder: (context, value, child) {
+                  print('Incrementing Counter2');
+                  return Text(
+                    value.toString(),
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  );
+                },
+                selector: (context, provider) => provider.counter2,
+              ),
+              Divider(thickness: 2, color: Colors.black),
+              Selector<HomeViewProvider, int>(
+                builder: (context, value, child) {
+                  print('Updating Total Counter');
+                  return Text(
+                    'Total Counter: $value',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  );
+                },
+                selector: (context, provider) => provider.totalCounter,
+              ),
+              SizedBox(height: 20),
+              Consumer(
+                builder: (context, HomeViewProvider provider, child) {
+                  return Row(
+                    mainAxisAlignment: .center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.deepPurple,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 24,
+                          ),
+                        ),
+                        onPressed: () {
+                          provider.incrementCounter1();
+                        },
+                        child: Text('Increment Counter1'),
+                      ),
+                      SizedBox(width: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.deepPurple,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 24,
+                          ),
+                        ),
+                        onPressed: () {
+                          provider.incrementCounter2();
+                        },
+                        child: Text('Increment Counter2'),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              SizedBox(height: 20),
+              Consumer<HomeViewProvider>(
+                builder: (context, provider, child) {
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 24,
+                      ),
+                    ),
+                    onPressed: () {
+                      provider.resetCounters();
+                    },
+                    child: Text('Reset Counters'),
+                  );
+                },
+              ),
             ],
-          ),)
+          ),
         ),
       ),
     );
   }
 }
 
-class HomeViewProvider extends ChangeNotifier{
-  String helloMessage = "Hello, World!";
-  void updateMessage(String newMessage){
-    helloMessage = newMessage;
+class HomeViewProvider extends ChangeNotifier {
+  int counter1 = 0;
+  int counter2 = 0;
+  int get totalCounter => counter1 + counter2;
+
+  void incrementCounter1() {
+    counter1++;
+    notifyListeners();
+  }
+
+  void incrementCounter2() {
+    counter2++;
+    notifyListeners();
+  }
+
+  void resetCounters() {
+    counter1 = 0;
+    counter2 = 0;
     notifyListeners();
   }
 }
